1070f8a9d85cce367f70b27a1e5c56590461f708
bdullah386 <bdullahbdullah386@gmail.com>
2022-08-06 10:46:13 +0700

    evaluasi

---
+++ main.js
@@ -0,0 +1,86 @@
+//jawaban no 1
+//== = yaitu sama dengan sedangkan
+//=== = sama dengan dan sama tipe (jenis nya sama)
+
+//jawaban no 2 
+//ternary
+// voteable = (age < 18) ? "Too young":"Old enough";
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// jawaban no 3 
+// let dataFilm = [
+//     {
+//         id: 1,
+//         title:'tragedi atap jebol',
+//         genre: ["action","romance","comedy","horor"],
+//         actor:["hanip","arull","nabil","moezha","angga"],
+//         ratting: 9.05,
+//         synopsis: 'lorem ipsum dolor sit amet, consectetur adipiscing elit,',
+//         type : 'movie', //movie // tv
+//         producer: ["PIT movic","hanip music live"],
+//         studio:'pesantrenIT',
+//         status: true // true => sudah rilis // false => belum rilis 
+
+
+//     }
+// ]
+
+// let pesantrenIT = { 
+//     id: 5,
+//     title:'tragedi tangki bocor',
+//     genre: ["action","romance","comedy","horor"],
+//     actor:["hanip","arull","nabil","moezha","angga"],
+//     ratting: 8.50,
+//     synopsis: 'lorem ipsum dolor sit amet, consectetur adipiscing elit,',
+//     type : 'movie', //movie // tv
+//     producer: ["PIT movic","hanip music live"],
+//     studio:'pesantrenIT',
+//     status: true // true => sudah rilis // false => belum rilis 
+// }
+
+// dataFilm.unshift(pesantrenIT)
+
+// // console.log(dataFilm);
+
+// //hapus data
+ 
+// dataFilm.shift(1)
+
+
+// console.log(dataFilm);
+
+let dataFilm = [
+    {
+        id: 1,
+        title:'tragedi atap jebol',
+        genre: ["action","romance","comedy","horor"],
+        actor:["hanip","arull","nabil","moezha","angga"],
+        ratting: 9.05,
+        synopsis: 'lorem ipsum dolor sit amet, consectetur adipiscing elit,',
+        type : 'movie', //movie // tv
+        producer: ["PIT movic","hanip music live"],
+        studio:'pesantrenIT',
+        status: true // true => sudah rilis // false => belum rilis 
+
+
+    }
+]
+console.log([0][3]);
